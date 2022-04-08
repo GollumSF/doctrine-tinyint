6a61cac20f2ffeb82b8eafffef6c1c88f4cb2999
Damien Duboeuf <smeagolworms4@gmail.com>
2022-04-08 02:36:25 +0200

    Update TinyInt.php

--- TinyInt.php
+++ TinyInt.php
@@ -8,10 +8,18 @@ use Doctrine\DBAL\Types\Type;
 class TinyInt extends Type {
 	const TINYINT = 'tinyint';
 	
+	/**
+	 * @param array $fieldDeclaration
+	 * @param AbstractPlatform $platform
+	 * @return string
+	 */
 	public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) {
 		return 'TINYINT' . ( ! empty($fieldDeclaration['unsigned']) ? ' UNSIGNED' : '');
 	}
 	
+	/**
+	 * @return string
+	 */
 	public function getName() {
 		return self::TINYINT;
 	}
